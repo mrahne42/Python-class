0278bdc78c567c7676357a6c584911f9b055eacd
Michael Rahne <mrahne4@gmail.com>
2025-09-13 16:13:51 -0400

    Add files via upload

---
+++ triangles.py
@@ -0,0 +1,65 @@
+# FILE NAME: triangles.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+#
+##
+###
+####
+#####
+
+    #
+   ##
+  ###
+ ####
+#####
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. This is your second "real" lab. What is one thing that you learned
+   from your previous labs that helped you in this lab?
+
+
+
+
+
+'''
