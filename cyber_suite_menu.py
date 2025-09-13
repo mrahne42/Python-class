1af1054a9684b349aa516eb10c158bc592248626
Michael Rahne <mrahne4@gmail.com>
2025-09-13 16:08:27 -0400

    Add files via upload

---
+++ cyber_suite_menu.py
@@ -0,0 +1,65 @@
+# FILE NAME: cyber_suite_menu.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+    
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+WELCOME TO THE cyber_suite
+
+Please choose from the following menu:
+  1. Password manager
+  2. Examine logs
+  3. Scrape website
+  4. Hash
+  5. Encrypt
+  6. Encode
+  7. Steganography
+  8. EXIT
+
+Enter selection:
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+1. This is the last exercise for the chapter! Imagine you are talking to a new Python learner. What
+   advice do you have for someone who is just getting started and getting ready to do their first lab?
+
+
+
+
+
+'''
