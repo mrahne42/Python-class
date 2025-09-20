010f568e91c504b8631ddec23b3b69c3dca0072f
Michael Rahne <mrahne4@gmail.com>
2025-09-20 14:34:22 -0400

    Add files via upload

---
+++ glue_name.py
@@ -0,0 +1,84 @@
+# FILE NAME - glue_name.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. You can see the Sample Output below, too
+
+
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+Enter first name: Dave
+Enter last name: Ghidiu
+Hi, Dave Ghidiu
+'''
+
+
+
+'''
+Enter first name: bart
+Enter last name: simpson
+Hi, bart simpson
+'''
+
+
+
+'''
+Enter first name: Paige
+Enter last name: LaBarr
+Hi, Paige LaBarr
+'''
+
+
+
+'''
+Enter first name: Lisa
+Enter last name: Simpson, esquire
+Hi, Lisa Simpson, esquire
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is a lesson you learned during this lab? You do not have to restrict
+   your response to Python. It might be a lesson in time management, a lesson
+   using CodeGrade, or something else.
+
+
+
+
+
+'''
