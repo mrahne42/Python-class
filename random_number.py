1d339c0b45bcf8d175a3de02a2830fe30b7ec201
Michael Rahne <mrahne4@gmail.com>
2025-09-27 11:58:02 -0400

    Add files via upload

---
+++ random_number.py
@@ -0,0 +1,77 @@
+# FILE NAME - random_number.py
+
+# NAME:
+# DATE:
+# BRIEF DESCRIPTION:
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+import random
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+def main():
+    generate_random_number()
+
+def generate_random_number():
+    ## TODO: Replace the code below with your code
+    print("Your code goes here")
+
+
+main()
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+0.8525041884365221
+
+'''
+
+
+
+'''
+
+0.13938116419681712
+
+'''
+
+
+
+'''
+
+0.6717733648463142
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. Why is your output most certainly different than the sample output?
+
+
+
+
+
+
+
+'''
