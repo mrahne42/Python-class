0cc1ec90f1abc2a686674de5f623a0de89a6527c
Michael Rahne <mrahne4@gmail.com>
2025-09-27 10:24:06 -0400

    Add files via upload

---
+++ random_letter.py
@@ -0,0 +1,70 @@
+# Starter code for `random_letter.py`
+
+import random
+import string
+
+# TODO
+
+# Write a program that:
+#   1. Asks the user for a number to use as a random seed.
+#   2. Seed a random number generator
+#   3. Use the random number generator to generate a random letter.
+
+# Be sure to have a main() method that calls another method to do the heavy lifting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+-----------------------
+RANDOM LETTER GENERATOR
+-----------------------
+
+Enter a seed for the random number generator: 33
+The random letter is: s
+
+'''
+
+
+
+'''
+
+-----------------------
+RANDOM LETTER GENERATOR
+-----------------------
+
+Enter a seed for the random number generator: 32
+The random letter is: c
+
+'''
+
+
+
+'''
+
+-----------------------
+RANDOM LETTER GENERATOR
+-----------------------
+
+Enter a seed for the random number generator: 32
+The random letter is: c
+
+'''
