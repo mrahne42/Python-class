728c9efce205d136ac097b98c00bd55e4b9c53ce
Michael Rahne <mrahne4@gmail.com>
2025-09-27 16:33:56 -0400

    Add files via upload

---
+++ bounded_random_number.py
@@ -0,0 +1,78 @@
+# FILE NAME - bounded_random_number.py
+
+# NAME:
+# DATE:
+# BRIEF DESCRIPTION:
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+import random
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+def main():
+    bounded_random()
+
+def bounded_random():
+    ## TODO: Replace the code below with your code
+    print("Your code goes here")
+
+main()
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Enter a seed for the random number generation: 33
+10
+
+'''
+
+
+
+'''
+
+Enter a seed for the random number generation: 32
+2
+
+'''
+
+
+
+'''
+
+Enter a seed for the random number generation: 100
+3
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is a good way to remember if the arguments (parameters) for a bounded number are inclusive or exclusive?
+
+
+
+
+
+
+'''
