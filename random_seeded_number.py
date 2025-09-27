aa7c8c2abda1ed1e96925fb92026b8a8b507c729
Michael Rahne <mrahne4@gmail.com>
2025-09-27 14:00:49 -0400

    Add files via upload

---
+++ random_seeded_number.py
@@ -0,0 +1,80 @@
+# FILE NAME - random_seeded_number.py
+
+# NAME:
+# DATE:
+# BRIEF DESCRIPTION:
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+import random
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+def main():
+    generate_seeded_random_number()
+
+def generate_seeded_random_number():
+    ## TODO: Replace the code below with your code
+    print("Your code goes here")
+
+
+main()
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Enter a seed for the random number generation: 33
+0.5703284231368732
+
+'''
+
+
+
+'''
+
+Enter a seed for the random number generation: 0
+0.8444218515250481
+
+'''
+
+
+
+'''
+
+Enter a seed for the random number generation: 10
+0.5714025946899135
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. In your own words, explain what "seeded" means (for instance, "seeding a random number generator").
+
+
+
+
+
+
+
+'''
