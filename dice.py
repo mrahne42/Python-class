266ab12484ebb6c105308c10a27c4f19424dc625
Michael Rahne <mrahne4@gmail.com>
2025-09-28 20:56:20 -0400

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,82 @@
+# FILE NAME - dice.py
+
+# NAME:
+# DATE:
+# BRIEF DESCRIPTION:
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+import random
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+def main():
+    roll_die()
+
+def roll_die():
+    ## TODO: Replace the code below with your code
+    print("Your code goes here")
+
+
+main()
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Enter a seed for the random number generation: 33
+Die roll one is 5
+Die roll two is 2
+
+'''
+
+
+
+'''
+
+Enter a seed for the random number generation: 22
+Die roll one is 2
+Die roll two is 2
+
+'''
+
+
+
+'''
+
+Enter a seed for the random number generation: -30
+Die roll one is 5
+Die roll two is 3
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is the purpose of "seeding" the random number generator?
+
+
+
+
+
+
+'''
