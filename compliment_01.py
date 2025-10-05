812d1333772f96fe855185ee164a870821d4ebfb
Michael Rahne <mrahne4@gmail.com>
2025-10-05 14:48:57 -0400

    Add files via upload

---
+++ compliment_01.py
@@ -0,0 +1,81 @@
+# FILE NAME - compliment_01.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+Would you like a compliment? yes
+You have wonderful eyes.
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? y
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? Yes
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? no
+Thank you for playing.
+'''
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. On a scale of 1 to 10 (where 10 is the hardest), how would you rate this lab?
+
+
+
+
+2. What was the hardest part of this lab?
+
+
+
+
+
+
+
+'''
