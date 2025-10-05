e39ece75caf1c29ed4f73a8626ab5c434a051938
Michael Rahne <mrahne4@gmail.com>
2025-10-05 12:51:02 -0400

    Add files via upload

---
+++ cm_and_inch_converter.py
@@ -0,0 +1,13 @@
+
+# FILE NAME: cm_and_inch_converter.py
+# AUTHOR: 
+# DATE: 
+# DESCRIPTION: 
+
+def main():
+    cm_and_inch_converter()
+
+def cm_and_inch_converter():
+    print('hello world')
+
+main()
