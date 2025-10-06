23ded2f983b9f694b325cc2bf2bc0fc180e0dcb9
Michael Rahne <mrahne4@gmail.com>
2025-10-06 12:19:33 -0400

    Add files via upload

---
+++ convert_C_to_F_02.py
@@ -0,0 +1,102 @@
+# FILE NAME - convert_C_to_F_02.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+===== Temperature Converter =====
+
+  1. Convert from Celsius to Fahrenheit
+  2. Convert from Fahrenheit to Celsius
+
+Please choose from the above menu: 1
+Enter a temperature to convert: 100
+
+100.0 degrees Celsius is 212.0 degrees Fahrenheit.
+'''
+
+
+'''
+===== Temperature Converter =====
+
+  1. Convert from Celsius to Fahrenheit
+  2. Convert from Fahrenheit to Celsius
+
+Please choose from the above menu: 2
+Enter a temperature to convert: 32
+
+32.0 degrees Fahrenheit is 0.0 degrees Celsius.
+'''
+
+
+'''
+===== Temperature Converter =====
+
+  1. Convert from Celsius to Fahrenheit
+  2. Convert from Fahrenheit to Celsius
+
+Please choose from the above menu: 1
+Enter a temperature to convert: -40
+
+-40.0 degrees Celsius is -40.0 degrees Fahrenheit.
+'''
+
+
+'''
+===== Temperature Converter =====
+
+  1. Convert from Celsius to Fahrenheit
+  2. Convert from Fahrenheit to Celsius
+
+Please choose from the above menu: 2
+Enter a temperature to convert: -40
+
+-40.0 degrees Fahrenheit is -40.0 degrees Celsius.
+'''
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is one lesson you learned in this lab?
+
+
+
+
+
+
+
+'''
